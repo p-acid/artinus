34afc332327b38b0632193cb17cb09defdb100b4
p-acid <qkrtkstjd01@gmail.com>
2025-09-02 16:14:44 +0900

    fix: safari font 오류 수정

--- src/app/styles/font.css.ts
+++ src/app/styles/font.css.ts
@@ -2,6 +2,18 @@ import { globalFontFace } from "@vanilla-extract/css";
 
 export const font = "Pretendard";
 
+// Regular
 globalFontFace(font, {
-  src: 'url("/fonts/PretendardVariable.ttf") format("ttf")',
+  src: 'url("/fonts/Pretendard-Regular.woff") format("woff")',
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontDisplay: "swap",
+});
+
+// SemiBold
+globalFontFace(font, {
+  src: 'url("/fonts/Pretendard-SemiBold.woff") format("woff")',
+  fontWeight: 700,
+  fontStyle: "normal",
+  fontDisplay: "swap",
 });
